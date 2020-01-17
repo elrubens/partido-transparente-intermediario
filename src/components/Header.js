5fb8c46842b6217be26b80f6e1a884010ae48607
Ruben Martinez <ruben.martinez@docomodigital.com>
2020-01-17 22:08:39 +0100

    test google analytics without gatsby plugin

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,16 @@ const Header = (props) => (
         <p>El amanecer de una nueva democracia<br />
         design by <a href="https://html5up.net">HTML5 UP</a>.</p>
     </header>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156318166-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156318166-1');
+</script>
+
 )
 
 export default Header
